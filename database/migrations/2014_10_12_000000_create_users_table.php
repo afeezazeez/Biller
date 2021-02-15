962f68f744d56a46b9675b0c0ae8aadc296f93a1
azeezafeez <chillingloccin1@gmail.com>
2021-02-15 23:42:03 +0100

    migrating users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->float('billing_amount');
+            $table->boolean('billing_status')->default(0);
+            $table->string('billing_provider');
+            $table->string('billed_by')->nullable()->default("");
         });
     }
 
